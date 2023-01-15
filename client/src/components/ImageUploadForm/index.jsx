861370e5b86e69ad269ffbf35c37989fa2035543
hafezfhmi <hafezfahmi2000@gmail.com>
2023-01-15 22:43:33 +0800

    Feat: Client - Add error handling to image upload

--- client/src/components/ImageUploadForm/index.jsx
+++ client/src/components/ImageUploadForm/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useField } from "../../hooks/index";
-import { useFileDrop } from "../../hooks/index";
+import { useFileDrop, useError } from "../../hooks/index";
 import FileDrop from "../fileDrop";
 import TextInput from "../TextInput.js";
 import Button from "../Button";
+import ErrorModal from "../ErrorModal";
 import imageService from "../../services/images";
 import styles from "./imageUploadForm.module.css";
 
@@ -11,6 +12,7 @@ const ImageUploadForm = () => {
   const title = useField("text", "title");
   const description = useField("", "description");
   const fileDrop = useFileDrop();
+  const error = useError();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -21,22 +23,33 @@ const ImageUploadForm = () => {
         description.attributes.value,
         fileDrop.value.image
       );
-    } catch (error) {
-      // TODO: ERROR HANDLING
+    } catch (err) {
+      error.handleError(true, err?.response?.data?.msg || err.message);
     }
   };
 
   return (
-    <form className={styles.imageUploadForm} onSubmit={handleSubmit}>
-      <div className={styles.fileDropWrapper}>
-        <FileDrop fileDrop={fileDrop} />
-      </div>
-      <div className={styles.inputWrapper}>
-        <TextInput field={title} label="Title" />
-        <TextInput field={description} label="Description" type="textarea" />
-        <Button label="Upload" style={{ marginTop: "1.2rem", width: "100%" }} />
-      </div>
-    </form>
+    <>
+      {error.error && (
+        <ErrorModal
+          errorMsg={error.errorMsg}
+          disableError={error.disableError}
+        />
+      )}
+      <form className={styles.imageUploadForm} onSubmit={handleSubmit}>
+        <div className={styles.fileDropWrapper}>
+          <FileDrop fileDrop={fileDrop} />
+        </div>
+        <div className={styles.inputWrapper}>
+          <TextInput field={title} label="Title" />
+          <TextInput field={description} label="Description" type="textarea" />
+          <Button
+            label="Upload"
+            style={{ marginTop: "1.2rem", width: "100%" }}
+          />
+        </div>
+      </form>
+    </>
   );
 };
 
