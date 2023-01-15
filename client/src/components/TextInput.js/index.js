bfdede4f9add52778d99247fe9004fef0e7e1e1f
hafezfhmi <hafezfahmi2000@gmail.com>
2023-01-15 17:36:57 +0800

    Feat: Client - TextInput component now can have textarea input

--- client/src/components/TextInput.js/index.js
+++ client/src/components/TextInput.js/index.js
@@ -1,13 +1,28 @@
 import React from "react";
 import styles from "./TextInput.module.css";
 
-const TextInput = ({ field, label }) => {
+const TextInput = ({ field, label, type = "input" }) => {
+  let inputMarkup;
+
+  switch (type) {
+    case "textarea":
+      inputMarkup = (
+        <textarea className={styles.textInput} {...field.attributes} />
+      );
+      break;
+    default:
+      inputMarkup = (
+        <input className={styles.textInput} {...field.attributes} />
+      );
+  }
+
   return (
     <div className={styles.inputWrapper}>
       <div className={styles.labelWrapper}>
         <label htmlFor={field.attributes.id}>{label}: </label>
         {field.error && <p>{field.error}</p>}
       </div>
+      {inputMarkup}
       <input {...field.attributes} />
     </div>
   );
