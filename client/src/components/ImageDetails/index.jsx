6dc7d28e2fa9683158ca7e42760d8e560577ec6b
hafezfhmi <hafezfahmi2000@gmail.com>
2023-01-20 22:56:55 +0800

    Feat: Client - Show modal that will later ask the user to login when clicked on likes or add comment

--- client/src/components/ImageDetails/index.jsx
+++ client/src/components/ImageDetails/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
 import { FcLike } from "react-icons/fc";
 import TextInput from "../TextInput.js";
@@ -7,15 +7,30 @@ import { useField } from "../../hooks/index.js";
 import userService from "../../services/users";
 import imageService from "../../services/images";
 import commentService from "../../services/comment.js";
+import { UseUserContext } from "../../context/userContext.js";
 import styles from "./imageDetails.module.css";
 import person from "../../assets/person-1.jpg";
+import LoginModal from "../LoginModal/index.jsx";
 
 const ImageDetails = ({ image }) => {
   const [user, setUser] = useState(null);
   const [likesAmount, setLikesAmount] = useState(0);
   const [liked, setLiked] = useState(false);
   const [comments, setComments] = useState([]);
+  const [showLogin, setShowLogin] = useState(false);
+
+  const userCtx = UseUserContext();
+
   const comment = useField("", "comment");
+
+  const handleOnClickLogin = useCallback(() => {
+    if (!userCtx.isLoggedIn) {
+      setShowLogin(true);
+      return true;
+    }
+
+    return false;
+  }, [userCtx.isLoggedIn]);
 
   // get user data
   useEffect(() => {
@@ -46,6 +61,12 @@ const ImageDetails = ({ image }) => {
   }, [image.id]);
 
   const handleLikes = () => {
+    const loginShowed = handleOnClickLogin();
+
+    if (loginShowed) {
+      return;
+    }
+
     try {
       imageService.likeOne(image.id);
       setLiked((prev) => !prev);
@@ -63,6 +84,12 @@ const ImageDetails = ({ image }) => {
   const handleRootCommmentSubmit = (e) => {
     e.preventDefault();
 
+    const loginShowed = handleOnClickLogin();
+
+    if (loginShowed) {
+      return;
+    }
+
     // TODO: Add functionality to refer to parent comment id, maybe move the comment to its own component. But this one is the root reply comment so parentId can be null
     commentService.create(comment.attributes.value, image.id, null);
   };
@@ -79,48 +106,51 @@ const ImageDetails = ({ image }) => {
   }, [image.id]);
 
   return (
-    <div className={styles.imageDetails}>
-      <h1>{image.title}</h1>
-      <p>{image.description}</p>
+    <>
+      {showLogin && <LoginModal disableError={() => setShowLogin(false)} />}
+      <div className={styles.imageDetails}>
+        <h1>{image.title}</h1>
+        <p>{image.description}</p>
 
-      <div className={styles.user}>
-        <img src={user?.userProfile || person} alt="" />
-        <Link to={`/user/${user?.id}`}>{user?.username}</Link>
-      </div>
+        <div className={styles.user}>
+          <img src={user?.userProfile || person} alt="" />
+          <Link to={`/user/${user?.id}`}>{user?.username}</Link>
+        </div>
 
-      <div className={styles.likeContainer}>
-        <FcLike className={!liked && styles.unliked} onClick={handleLikes} />
-        <p>{likesAmount} Likes</p>
-      </div>
+        <div className={styles.likeContainer}>
+          <FcLike className={!liked && styles.unliked} onClick={handleLikes} />
+          <p>{likesAmount} Likes</p>
+        </div>
 
-      <form
-        className={styles.commentFormContainer}
-        onSubmit={handleRootCommmentSubmit}
-      >
-        <TextInput field={comment} label={"Comment"} type="textarea" />
-        <Button label="Add comment" style={{ marginTop: "1.2rem" }} />
-      </form>
+        <form
+          className={styles.commentFormContainer}
+          onSubmit={handleRootCommmentSubmit}
+        >
+          <TextInput field={comment} label={"Comment"} type="textarea" />
+          <Button label="Add comment" style={{ marginTop: "1.2rem" }} />
+        </form>
 
-      <div className={styles.commentListContainer}>
-        <ul>
-          {comments.map((comment) => (
-            <li key={comment.id} className={styles.commentList}>
-              <img src={user?.userProfile || person} alt="" />
-              <div className={styles.commentListDetails}>
-                <p>{comment.user.username}</p>
-                <p>{comment.detail}</p>
+        <div className={styles.commentListContainer}>
+          <ul>
+            {comments.map((comment) => (
+              <li key={comment.id} className={styles.commentList}>
+                <img src={user?.userProfile || person} alt="" />
+                <div className={styles.commentListDetails}>
+                  <p>{comment.user.username}</p>
+                  <p>{comment.detail}</p>
 
-                <div className={styles.commentAction}>
-                  <p>Reply</p>
-                  <p>Edit</p>
-                  <p>Like</p>
+                  <div className={styles.commentAction}>
+                    <p>Reply</p>
+                    <p>Edit</p>
+                    <p>Like</p>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
