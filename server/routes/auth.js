a82ab101943e8f8b1a815eca97f474dd3f31aa10
hafezfhmi <hafezfahmi2000@gmail.com>
2023-01-30 17:10:30 +0800

    Refactor: Server - refactor auth router's routes definition order

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,22 +1,23 @@
 const router = require("express").Router();
 
 const {
+  getRelog,
+  getOneResetPassword,
   postLogin,
   postLogout,
   postSignup,
-  getRelog,
   postResetPassword,
-  getOneResetPassword,
   postNewPassword,
 } = require("../controllers/auth");
 const isAuth = require("../utils/isAuth");
 
+router.get("/relog", isAuth, getRelog);
+router.get("/resetPassword/:resetToken", getOneResetPassword);
+
 router.post("/login", postLogin);
 router.post("/logout", isAuth, postLogout);
 router.post("/signup", postSignup);
-router.get("/relog", isAuth, getRelog);
 router.post("/resetPassword", postResetPassword);
-router.get("/resetPassword/:resetToken", getOneResetPassword);
 router.post("/resetPassword/:resetToken", postNewPassword);
 
 module.exports = router;
