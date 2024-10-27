a6a69641f2af65937d063911c5e168908a1ccdc9
hafezfhmi <hafezfahmi2000@gmail.com>
2024-10-27 14:08:59 +0800

    fix: remove default http protocol in baseUrl

--- client/src/services/user.js
+++ client/src/services/user.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = `http://${process.env.REACT_APP_BACKEND_URL || "http://localhost:3001"}/user`;
+const baseUrl = `${process.env.REACT_APP_BACKEND_URL || "http://localhost:3001"}/user`;
 
 const getUser = (id) => {
   return axios.get(`${baseUrl}/${id}`).then((response) => response.data);
